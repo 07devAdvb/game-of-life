03f49208c42e85256d9e872ea12442685a84eacf
The-dev-07 <davidvivarbogonez@gmail.com>
2024-07-28 11:33:34 +0200

    Cleaned the code, better readability, and improved performance

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 )
 
 const (
-	screenWidth  = 1000
-	screenHeight = 1000
+	screenWidth  = 1200
+	screenHeight = 800
 	cellSize     = 10
 	gridWidth    = screenWidth / cellSize
 	gridHeight   = screenHeight / cellSize
@@ -19,57 +19,77 @@ const (
 
 type Game struct {
 	Cells     [][]bool
-	TempCells [][]bool
+	NextCells [][]bool
 }
 
 func NewGame() *Game {
+	// Initialize the rows of each set of cells
 	g := &Game{
 		Cells:     make([][]bool, gridHeight),
-		TempCells: make([][]bool, gridHeight),
+		NextCells: make([][]bool, gridHeight),
 	}
+	// Initialize each cell of each row to a random state
 	for i := range g.Cells {
 		g.Cells[i] = make([]bool, gridWidth)
-		g.TempCells[i] = make([]bool, gridWidth)
+		g.NextCells[i] = make([]bool, gridWidth)
 		for j := range g.Cells[i] {
 			g.Cells[i][j] = rand.Intn(2) == 0
 		}
 	}
+	// Return the initialized game
 	return g
 }
 
+// Traverse each cell and update its state based on its neighbors
 func (g *Game) Update() error {
 	for y := 0; y < gridHeight; y++ {
 		for x := 0; x < gridWidth; x++ {
 			count := g.countNeighbors(x, y)
-			g.TempCells[y][x] = (count == 3) || (count == 2 && g.Cells[y][x])
+			g.NextCells[y][x] = (count == 3) || (count == 2 && g.Cells[y][x])
 		}
 	}
-	g.Cells, g.TempCells = g.TempCells, g.Cells
+	g.Cells, g.NextCells = g.NextCells, g.Cells
 	return nil
 }
+
+// Draw the current state of the game with a white square size 10
+// The size of the  grid is the size of the screen divided by the size of a cell
 func (g *Game) Draw(screen *ebiten.Image) {
 	for y := 0; y < gridHeight; y++ {
 		for x := 0; x < gridWidth; x++ {
+			// If the cell is alive, draw a white square
 			if g.Cells[y][x] {
-				vector.DrawFilledRect(screen, float32(x*cellSize), float32(y*cellSize), float32(cellSize), float32(cellSize), color.White, false)
+				vector.DrawFilledRect(
+					screen,
+					float32(x*cellSize), float32(y*cellSize), // Position of the top left corner of the rectangle
+					float32(cellSize), float32(cellSize), // Size of the rectangle
+					color.White,
+					false,
+				)
 			}
 		}
 	}
 }
 
+// Return the size of the screen
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
+// Count the number of neighbors of a cell by looking at the cells surrounding it
 func (g *Game) countNeighbors(x, y int) int {
 	count := 0
+	// Start at the cell to the left and right of the current cell
 	for i := -1; i <= 1; i++ {
+		// Start at the cell above and below the current cell
 		for j := -1; j <= 1; j++ {
 			if i == 0 && j == 0 {
 				continue
 			}
+			// Calculate the index of the neighboring cell
 			x2 := (x + j + gridWidth) % gridWidth
 			y2 := (y + i + gridHeight) % gridHeight
+			// If the cell to the left and right of the current cell is alive, increment the count
 			if g.Cells[y2][x2] {
 				count++
 			}
@@ -84,6 +104,7 @@ func main() {
 
 	g := NewGame()
 
+	// Frame rate
 	ebiten.SetTPS(10)
 
 	if err := ebiten.RunGame(g); err != nil {
